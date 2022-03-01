2b4f473dd77428549f9c6e2074ca5e12c64d8dd2
cgooch99 <97248270+cgooch99@users.noreply.github.com>
2022-03-01 17:32:18 -0600

    Add files via upload

--- script/ftrot.js
+++ script/ftrot.js
@@ -1,3 +1,58 @@
-$(document).ready(function(){ 
+
+$(function () {
+    console.log("ready!");
+
+    // $('#myThings').append(value)
+
+    $.each(stuffIDo, function(index, value) {
+
+        console.log(index, value);
+            $('#myThings').append("<li>" + value + "</li>");
+
+        // find my codepen example, story card
+        // we are currently in a loop: $().each ... 
+        // loop through stuffIDo array
+        // every "turn", print the values.. .eating food
+        // appending to your exisiting list #myThings.. 
+
+    }); 
+
+});
+
+const stuffIDo = [
+
+    "Playing Basketball", 
+    "Playing Guitar",
+    "Fishing",
+    "Watching Netflix"
+]
+
+const goochData = {
+    teley: [
+      { showName: "Supernatural", venue: "Netflix" },
+      { showName: "The Boys", venue: "AmazonPrime" },
+      { showName: "Alabama Football", venue: "SEC Network" }
+    ],
+    music: [
+      { artistName: "Luke Combs" },
+      { artistName: "Keith Whitely" },
+      { artistName: "Foo Fighter" }
+    ]
+  };
+
+    $.each(goochData, function (key, item) {
+      $("#stuff-lists").append(`<h4> ${key} </h4> <ul id="-list${key}">`);
+      $.each(item, function (index, val) {
+        //$(`#${key}-list`).append(val.propertyName);
+        $.each(val, function (propertyName, propertyValue) {
+          console.log(propertyName, propertyValue);
+
+          $(`#${key}-list`).append(`<li> ${propertyValue}`);
+  
+        });
+      });
+    });
+  
+
     
-    console.log("doc is ready")});
+    

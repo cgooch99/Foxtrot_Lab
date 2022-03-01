5b3025e107612cd6e21388f35bb071da40bb87f5
cgooch99 <97248270+cgooch99@users.noreply.github.com>
2022-03-01 17:44:53 -0600

    Add files via upload

--- script/ftrot.js
+++ script/ftrot.js
@@ -2,21 +2,12 @@
 $(function () {
     console.log("ready!");
 
-    // $('#myThings').append(value)
-
     $.each(stuffIDo, function(index, value) {
 
         console.log(index, value);
             $('#myThings').append("<li>" + value + "</li>");
 
-        // find my codepen example, story card
-        // we are currently in a loop: $().each ... 
-        // loop through stuffIDo array
-        // every "turn", print the values.. .eating food
-        // appending to your exisiting list #myThings.. 
-
     }); 
-
 });
 
 const stuffIDo = [
@@ -43,7 +34,6 @@ const goochData = {
     $.each(goochData, function (key, item) {
       $("#stuff-lists").append(`<h4> ${key} </h4> <ul id="-list${key}">`);
       $.each(item, function (index, val) {
-        //$(`#${key}-list`).append(val.propertyName);
         $.each(val, function (propertyName, propertyValue) {
           console.log(propertyName, propertyValue);
 
